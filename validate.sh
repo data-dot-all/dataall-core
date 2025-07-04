df38e33aded49903ae8b54f1b6d3d5dc2854bc96
kalosp <kalosp@amazon.com>
2025-07-04 13:01:32 +0300

    misc improvements

--- validate.sh
+++ validate.sh
@@ -4,5 +4,5 @@ set -ex
 ruff format --check .
 ruff check .
 mypy --install-types --non-interactive dataall_core
-pylint -j 0 --disable=all --enable=R0911,R0912,R0913,R0915 --fail-under=9 dataall_core
+pylint dataall_core
 poetry check --lock
